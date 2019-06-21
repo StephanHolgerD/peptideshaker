130228844e713ace185c29e4cbd7ff5e800b374e
StephanHolgerD <stephan.drukewitz@uni-leipzig.de>
2019-06-21 11:57:49 +0200

    script to add decoy and prevent super slow step in search gui

---
+++ decoy.py
@@ -0,0 +1,12 @@
+from Bio import SeqIO
+import sys
+input=sys.argv[1]
+output=input+"_decoy.fasta"
+with open(output, 'w') as s:
+    with open(input) as r:
+        for record in SeqIO.parse(r, "fasta"):
+            SeqIO.write(record, s, 'fasta')
+            record.seq=record.seq[::-1]
+            record.id=record.id+"_REVERSED"
+            record.description=record.id
+            SeqIO.write(record, s, 'fasta')
