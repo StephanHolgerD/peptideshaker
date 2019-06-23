62fc9624ff2a74279272a3fb40cd0cce1c285a9a
StephanHolgerD <stephan.drukewitz@uni-leipzig.de>
2019-06-23 12:16:51 +0200

    script to strip the stopcodons and add the decoy tag, peptide search gui will add a decaoy sequence (reverse sequence of target sequence) if they are missing. Script does the same as search gui but quicker, saves a lot of time when working with fasta > 100mb

--- decoy.py
+++ decoy.py
@@ -1,12 +1,15 @@
 from Bio import SeqIO
 import sys
 input=sys.argv[1]
+#input="seqs_for_3_8.fasta.transdecoder.awked.fasta"
 output=input+"_decoy.fasta"
 with open(output, 'w') as s:
     with open(input) as r:
         for record in SeqIO.parse(r, "fasta"):
+            record.seq=record.seq.rstrip("*")
+            record.description=record.id
             SeqIO.write(record, s, 'fasta')
             record.seq=record.seq[::-1]
-            #record.id=record.id+"_REVERSED"
-            record.description=record.description+"_REVERSED"
+            record.id=record.id+"_REVERSED"
+            record.description=record.id
             SeqIO.write(record, s, 'fasta')
